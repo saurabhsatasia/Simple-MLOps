441bc950af904ab29b8ce22f5cca73425b43567c
saurabhsatasia <saurabhsatasia@icloud.com>
2021-03-18 16:17:48 +0100

    setup done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="MLOps-demo package",
+    author="saurabhsatasia",
+    packages=find_packages(),
+    license="MIT",
+)
+
